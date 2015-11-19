050db00e206055d1ad724809f583a847a4fd5f92
Leonardo Borges Athayde Vasconcelos <leo.athayde@hotmail.com>
2015-11-19 17:04:05 -0200

    .

--- resources/views/layouts/contato.blade.php
+++ resources/views/layouts/contato.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
-	leonardo
+<div class="container" >
+	<div class="row" >
+		<div class="col-lg-1" style="background: #00f;"><div class="white-box">dsd</div></div>
+		<div class="col-lg-2" style="background: #fff;">d</div>
+		<div class="col-lg-3" >><div class="white-box">dsd</div></div>
+		<div class="col-lg-6 col-xs-3" style="background: #341240;">d</div>
+	</div>
+</div>
 
 @endsection
